29a702b8e594719969e332e98082a6755204c935
James <jh97uk@icloud.com>
2020-10-17 01:13:40 +0100

    Added Add Task Dialog. WIP

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 import TaskItem from './components/TaskItem.js';
+import AddEditTaskDialog from './views/AddEditTaskDialog.js';
 
-function App() {
-  return (
-    <Grid
-      container 
-      className="App"
-      direction='row'
-      justify='center'>
-      <Card 
-        component={Grid}
-        item
-        xs={7}
-        style={{marginTop:50, textAlign:'left'}}>
-        <Paper 
-          style={{width:'100%', backgroundColor:'rgb(156, 204, 100)', paddingLeft:15, paddingRight:15}} 
-          square>
-          <Typography variant="h4">Todo</Typography>
-        </Paper>
-          <TaskItem/>
-      </Card>
-    </Grid>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {todoItems:[], showTaskDialog:false}
+    this.onAddTaskClick = this.onAddTaskClick.bind(this);
+    this.addTaskDialogClose = this.addTaskDialogClose.bind(this);
+  }
+
+  onAddTaskClick(){
+    this.setState({showAddTaskDialog:true});
+  }
+
+  addTaskDialogClose(){
+    this.setState({showAddTaskDialog:false});
+  }
+
+  render(){
+    return (
+      <Grid
+        container 
+        className="App"
+        direction='row'
+        justify='center'>
+        <Card 
+          component={Grid}
+          item
+          xs={7}
+          style={{marginTop:50, textAlign:'left'}}>
+          <Paper 
+            style={{
+              width:'100%', 
+              backgroundColor:'rgb(156, 204, 100)', 
+              display:'flex',
+              justifyContent:'space-around',
+            }} 
+            square>
+            <Typography variant="h4" style={{marginLeft:15, marginTop:10, marginBottom:10}}>Todo</Typography>
+            <div style={{flexGrow:1}}></div>
+            <Button onClick={this.onAddTaskClick}>+</Button>
+          </Paper>
+            <TaskItem/>
+        </Card>
+        <AddEditTaskDialog open={this.state.showAddTaskDialog} close={this.addTaskDialogClose}/>
+      </Grid>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/AddEditTaskDialog.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+class AddEditTaskDialog extends Component{
+    constructor(props){
+        super(props);
+        this.state = {taskName:''};
+        this.onTaskNameChanged = this.onTaskNameChanged.bind(this);
+        this.addTask = this.addTask.bind(this);
+    }
+
+    onTaskNameChanged(event){
+        const value = event.target.value;
+        this.setState({taskName:value});
+    }
+
+    addTask(){
+        let currentTasks = localStorage.getItem('tasks');
+        if(currentTasks == ''){
+            localStorage.setItem('tasks', []);
+            currentTasks = [];
+        }
+        console.log(typeof currentTasks);
+        localStorage.setItem('tasks', currentTasks.push({done:false, name:this.state.taskName}));
+        this.props.close();
+    }
+
+    render(){
+        return(
+        <Dialog open={this.props.open}>
+            <DialogTitle>Add a new task</DialogTitle>
+            <DialogContent>
+                <TextField label="Task name" onChange={this.onTaskNameChanged} value={this.state.taskName} style={{width:'100%'}}/>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={this.props.close}>CANCEL</Button>
+                <Button onClick={this.addTask}>SAVE</Button>
+            </DialogActions>
+        </Dialog>)
+    }
+}
+
+export default AddEditTaskDialog;
