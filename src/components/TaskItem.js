9f2146e5a56c62b1e961265826868093695085ac
James <jh97uk@icloud.com>
2020-10-30 18:20:09 +0000

    Converted into functional component

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,24 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
 
-class TaskItem extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-
-        return (
-            <div style={{padding:15, display:'flex', justifyContent:'space-between'}}>
-                <Checkbox checked={this.props.task.done} onChange={()=>this.props.taskDone(this.props.task)}/>
-                <Typography variant="h6" display="inline" style={{verticalAlign:'middle', lineHeight:'inherit', display: 'flex', alignItems:'center'}}>{this.props.task.name}</Typography>
-                <div style={{flexGrow:1}}></div>
-                {this.props.task.done ? <Button onClick={()=>this.props.delete(this.props.task)}>Del</Button> : ''}
-            </div>)
-    }
+function TaskItem(props){
+    return (
+        <div style={{padding:15, display:'flex', justifyContent:'space-between'}}>
+            <Checkbox checked={props.task.done} onChange={()=>props.taskDone(props.task)}/>
+            <Typography variant="h6" display="inline" style={{verticalAlign:'middle', lineHeight:'inherit', display: 'flex', alignItems:'center'}}>{props.task.name}</Typography>
+            <div style={{flexGrow:1}}></div>
+            {props.task.done ? <Button onClick={()=>props.delete(props.task)}>Del</Button> : ''}
+        </div>)
 }
+
 
 export default TaskItem;
